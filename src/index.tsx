28369e464e585fd1e72cd473801060c292a8dd7c
Chad Hinds <chadrickj8@gmail.com>
2021-07-31 10:07:00 -0400

    Import createStore and applyMiddleware from redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux';
 
 
 ReactDOM.render(<App color="red" />, document.getElementById('root'));
 // NB: When using a class component in React wit Typescript you can pass in two generics: One for props, and one for state.
-    // If you do state this way, you overwrite state in the React.Component class
+// If you do state this way, you overwrite state in the React.Component class
