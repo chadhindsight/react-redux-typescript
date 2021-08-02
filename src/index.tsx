f87eb908b2cd4a5eb8e0a521a24d81f794bbe6d5
Chad Hinds <chadrickj8@gmail.com>
2021-08-02 12:02:53 -0400

    Import thunk and other redux related things that will be used

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
-
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 ReactDOM.render(<App color="red" />, document.getElementById('root'));
 // NB: When using a class component in React wit Typescript you can pass in two generics: One for props, and one for state.
